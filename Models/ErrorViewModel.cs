bc32c26dd51910b2899adef48f8b363c2896e9e6
jeduke <jethanduke@gmail.com>
2022-10-06 18:44:20 -0500

    base site template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gothportal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
