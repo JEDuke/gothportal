21e79f565ae4766ac20d55d21bbfc79906dd4583
jeduke <jethanduke@gmail.com>
2022-10-16 16:13:15 -0500

    call Api and return response message

--- web/Services/GothApiService.cs
+++ web/Services/GothApiService.cs
@@ -8,24 +8,72 @@ namespace gothportal.Services
 {
     public class GothApiService : IGothApiService
     {
-        static HttpClient client = new HttpClient();
-        
-        static async Task<dailyModel> GetProductAsync(string path)
+        public string GetMessage(string key)
         {
-            dailyModel dailyModel = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                dailyModel = await response.Content.ReadAsAsync<dailyModel>();
-            }
-            return dailyModel;
+            var message = RunAsync(key);
+            return message.Result;
         }
 
-        static void ShowProduct(dailyModel product)
+        HttpClient client = new HttpClient();
+        
+        private async Task<string> RunAsync(string keyId)
         {
-            Console.WriteLine($"Name: {product.Name}\tPrice: " +
-                $"{product.Price}\tCategory: {product.Category}");
+            //http://localhost:7071/api/imageUploader?name=hardtimesbefallensoulsurvivors
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:7071/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("api/imageUploader?name=" + keyId);
+
+                return await response.Content.ReadAsAsync<string>();
+
+                // var url = await CreateProductAsync(product);
+                // Console.WriteLine($"Created at {url}");
+
+                // // Get the product
+                // product = await GetProductAsync(id);
+                // ShowProduct(product);
+
+                // // Update the product
+                // Console.WriteLine("Updating price...");
+                // product.Price = 80;
+                // await UpdateProductAsync(product);
+
+                // // Get the updated product
+                // product = await GetProductAsync(url.PathAndQuery);
+                // ShowProduct(product);
+
+                // // Delete the product
+                // var statusCode = await DeleteProductAsync(product.Id);
+                // Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
+
+        // static async Task<dailyModel> GetProductAsync(string path)
+        // {
+        //     dailyModel dailyModel = null;
+        //     HttpResponseMessage response = await client.GetAsync(path);
+        //     if (response.IsSuccessStatusCode)
+        //     {
+        //         dailyModel = await response.Content.ReadAsAsync<dailyModel>();
+        //     }
+        //     return dailyModel;
+        // }
+
+        // static void ShowProduct(dailyModel product)
+        // {
+        //     Console.WriteLine($"Name: {product.Name}\tPrice: " +
+        //         $"{product.Price}\tCategory: {product.Category}");
+        // }
 
         // static async Task<Uri> CreateProductAsync(Product product)
         // {
@@ -54,62 +102,5 @@ namespace gothportal.Services
         //         $"api/products/{id}");
         //     return response.StatusCode;
         // }
-
-        static void Main()
-        {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync(string id)
-        {
-            // Update port # in the following line.
-            client.BaseAddress = new Uri("http://localhost:64195/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            try
-            {
-                // Create a new product
-                dailyModel product = new dailyModel
-                {
-                    Name = "Gizmo",
-                    Price = 100,
-                    Category = "Widgets"
-                };
-
-                // var url = await CreateProductAsync(product);
-                // Console.WriteLine($"Created at {url}");
-
-                // Get the product
-                product = await GetProductAsync(id);
-                ShowProduct(product);
-
-                // // Update the product
-                // Console.WriteLine("Updating price...");
-                // product.Price = 80;
-                // await UpdateProductAsync(product);
-
-                // // Get the updated product
-                // product = await GetProductAsync(url.PathAndQuery);
-                // ShowProduct(product);
-
-                // // Delete the product
-                // var statusCode = await DeleteProductAsync(product.Id);
-                // Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
-        }
-
-        public string GetMessage(string key)
-        {
-            return "Success! Key ID: " + key + ". ";
-        }
     }
 }
