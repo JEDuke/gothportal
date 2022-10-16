c31d9aa3d285c3942b3d3c77ff0364701f61186a
jeduke <jethanduke@gmail.com>
2022-10-16 15:34:53 -0500

    add dailyModel class

---
+++ web/Models/dailyModel.cs
@@ -0,0 +1,10 @@
+namespace gothPortal.Models
+{
+    public class dailyModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
