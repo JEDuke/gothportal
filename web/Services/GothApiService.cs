acf4efe63814fd0903ecf8bbd3122a26bcaa836d
jeduke <jethanduke@gmail.com>
2022-11-28 19:25:14 -0600

    add configuration service, remove unused code

--- web/Services/GothApiService.cs
+++ web/Services/GothApiService.cs
@@ -1,26 +1,25 @@
 using System.IO;
 using System.Net.Http;
 using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
 
 namespace gothportal.Services
 {
     public class GothApiService : IGothApiService
     {
-        public byte[] GetImage(string key)
-        {
-            var message = Run(key);
-            return message;
+        private readonly IConfiguration configuration;
+
+        public GothApiService(IConfiguration _configuration){
+            configuration = _configuration;
         }
-
-        HttpClient client = new HttpClient();
-
-        private byte[] Run(string keyId)
+        public byte[] GetImage(string name)
         {
-            string connString = "DefaultEndpointsProtocol=https;AccountName=gothstorage;AccountKey=2PVMTSyp1N3W98eGAGnp5D/5dRXlfrq8raJsPCSIoulHD+gA5nGAgrgvekriW2tcKAMnend4kS0n+AStaOmIpQ==;EndpointSuffix=core.windows.net";
+            string connString = configuration["blobStorageConnectionString"];
+            string blobContainerName = configuration["blobStorageContainerName"];
             Azure.Storage.Blobs.BlobClient blobClient = new BlobClient(
-                connString,
-                "gothportal",
-                keyId
+                connectionString: connString,
+                blobContainerName: blobContainerName,
+                blobName: name
             );
 
             using (MemoryStream ms = new MemoryStream())
@@ -28,23 +27,6 @@ namespace gothportal.Services
                 blobClient.DownloadTo(ms);
                 return ms.ToArray();
             }
-
-            // //http://localhost:7071/api/imageUploader?name=hardtimesbefallensoulsurvivors
-            // // Update port # in the following line.
-            // client.BaseAddress = new Uri("http://localhost:7071/");
-            // client.DefaultRequestHeaders.Accept.Clear();
-            // client.DefaultRequestHeaders.Accept.Add(
-            //     new MediaTypeWithQualityHeaderValue("application/json"));
-
-            // try
-            // {
-            //     HttpResponseMessage response = await client.GetAsync("api/imageUploader?name=" + keyId);
-            //     return await response.Content.ReadAsAsync<string>();
-            // }
-            // catch (Exception e)
-            // {
-            //     return e.Message;
-            // }
         }
     }
 }
